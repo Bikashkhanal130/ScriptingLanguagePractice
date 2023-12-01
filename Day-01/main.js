37fe8879aacd539acdd91fc4620f2686a9a79ca0
Bikashkhanal130 <khanalbk18@gmail.com>
2023-12-01 09:04:56 +0545

    variables

---
+++ Day-01/main.js
@@ -0,0 +1,8 @@
+    var sumOfTwoDigits=10; //regular case
+    var sumOfTwoDigits=10; //camel case 
+    var sum_Of_Two_Digits=10; //snake case
+    var sumOfTwoDigits=10; //pascal case
+   // var sum-of-two-digits=10; //kabab case
+    console.log(sumoftwodigits);
+//var a=4;
+console.log(a);
